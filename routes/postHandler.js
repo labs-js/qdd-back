623f062cf57ec41c3332927ae6363a1c7319c31d
Alan Campora <alan.campora@gmail.com>
2016-12-18 13:45:02 -0300

    [MOD] testing routes

--- routes/postHandler.js
+++ routes/postHandler.js
@@ -4,12 +4,16 @@ var configuration = require('../utils/configuration'),
 const postHandler = {};
 
 postHandler.getAll = function(req, res) {
-    Post.find({}, function(err, data) {
-        if (err) throw err;
-        console.log('new request:', new Date(), data);
-        // object of all the users
-        res.json(data);
-    });
+    res.json('app working as a charm!'); 
+    /*
+     * 
+     *Post.find({}, function(err, data) {
+     *    if (err) throw err;
+     *    console.log('new request:', new Date(), data);
+     *    // object of all the users
+     *    res.json(data);
+     *});
+     */
 }
 
 module.exports = postHandler;
